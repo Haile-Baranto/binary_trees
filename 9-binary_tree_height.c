47663f7e838cfae2d57f5e923a0d2871e4b3614b
Haile-Baranto <hailebaranto@gmail.com>
2023-08-03 14:41:58 +0300

    Facterize 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,20 +2,22 @@
 
 /**
  * binary_tree_height - Measures the height of a binary tree.
- *
- * @tree: Pointer to the root node of the tree.
+ * @tree: Pointer to the root node of the tree to measure the height.
  *
  * Return: Height of the binary tree. If tree is NULL, return 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return ((left_height > right_height) ? left_height : right_height);
 }
