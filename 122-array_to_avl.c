c813da087b08ab27b39ee7138805d3a756f6d4ee
Haile-Baranto <hailebaranto@gmail.com>
2023-08-04 02:16:02 +0300

    Add 122-array_to_avl.c

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,31 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - Builds an AVL tree from an array of integers.
- * @array: Pointer to the first element of the array to be converted.
+ * array_to_avl - Converts an array to an AVL tree.
+ * @array: Pointer to the first element of the array to
+ * be converted.
  * @size: The number of elements in the array.
  *
- * Return: A pointer to the root node of the created AVL tree,
- * or NULL on failure.
+ * Return: A pointer to the root node of the created AVL
+ * tree, or NULL on failure.
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	size_t i;
-	avl_t *root = NULL;
+    size_t i, j = 0;
+    avl_t *root;
 
-	if (size == 0 || array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		/* Insert unique elements into the AVL tree */
-		if (avl_insert(&root, array[i]) == NULL)
-		{
-			/* Free the already built AVL tree on failure */
-			avl_delete(root);
-			return (NULL);
-		}
-	}
-
-	return (root);
+    root = NULL;
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < size; i++)
+    {
+        /* Check for duplicates in the array */
+        for (j = 0; j < i; j++)
+        {
+            if (array[j] == array[i])
+                break;
+        }
+        /* Insert unique elements into the AVL tree */
+        if (j == i)
+        {
+            if (avl_insert(&root, array[i]) == NULL)
+            {
+                /* Free the already built AVL tree on failure */
+                return (NULL);
+            }
+        }
+    }
+    return (root);
 }
