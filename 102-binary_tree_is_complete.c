e4ae39c681b98a5fab321295aa001049b4f96a77
Haile-Baranto <hailebaranto@gmail.com>
2023-08-03 17:31:09 +0300

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,8 +1,23 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * is_complete - Recursive helper function to check if
- * a binary tree is complete.
+ * count_nodes - Count the number of nodes in a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree to count.
+ *
+ * Return: Number of nodes in the tree.
+ */
+int count_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
+}
+
+/**
+ * is_complete - Recursive helper function to check if a binary tree is complete.
  *
  * @tree: Pointer to the root node of the tree to check.
  * @index: Current index of the node.
@@ -19,7 +34,7 @@ int is_complete(const binary_tree_t *tree, int index, int count)
 		return (0);
 
 	return (is_complete(tree->left, 2 * index + 1, count) &&
-			is_complete(tree->right, 2 * index + 2, count));
+	        is_complete(tree->right, 2 * index + 2, count));
 }
 
 /**
@@ -31,17 +46,12 @@ int is_complete(const binary_tree_t *tree, int index, int count)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int count = 0;
-	const binary_tree_t *node = tree;
+	int count;
 
 	if (tree == NULL)
 		return (0);
 
-	while (node != NULL)
-	{
-		count++;
-		node = node->left;
-	}
+	count = count_nodes(tree);
 
 	return (is_complete(tree, 0, count));
 }
