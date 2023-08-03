6737bbebabe816dc390b9cefbc2851bc6f3abf5c
Haile-Baranto <hailebaranto@gmail.com>
2023-08-03 18:41:36 +0300

    remove binary_tree_remove function

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,24 +1,5 @@
+#include <stdlib.h>
 #include "binary_trees.h"
-
-/**
- * binary_tree_delete - Deletes an entire binary tree.
- *
- * @tree: Pointer to the root node of the tree to delete.
- */
-void binary_tree_delete(binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return;
-
-	/* Delete the left subtree */
-	binary_tree_delete(tree->left);
-
-	/* Delete the right subtree */
-	binary_tree_delete(tree->right);
-
-	/* Delete the current node */
-	free(tree);
-}
 
 /**
  * array_to_bst - Builds a Binary Search Tree from an array.
@@ -39,11 +20,7 @@ bst_t *array_to_bst(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		if (bst_insert(&root, array[i]) == NULL)
-		{
-			/* Free the already built tree on failure */
-			binary_tree_delete(root);
 			return (NULL);
-		}
 	}
 
 	return (root);
